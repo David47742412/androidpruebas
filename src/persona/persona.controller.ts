fca3dc6763668d6bca6ffe0f6433cd02cde0882b
David47742412 <davidcastroastudillo20@gmail.com>
2022-11-24 11:40:22 -0500

    cambios

--- src/persona/persona.controller.ts
+++ src/persona/persona.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { PersonaService } from './persona.service';
 import { CreatePersonaDto } from './dto/create-persona.dto';
 import { UpdatePersonaDto } from './dto/update-persona.dto';
@@ -11,73 +19,66 @@ import { UtilityService } from 'src/services/getHours.services';
 export class PersonaController {
   constructor(
     @InjectModel(Persona.name) private person: Model<PersonaDocument>,
-    private utily: UtilityService
-    ) {}
+    private utily: UtilityService,
+  ) {}
 
   @Post()
   async create(@Body() usuario: CreatePersonaDto) {
-
     try {
-      
       usuario.createdAt = this.utily.obtenerfechaactual();
       usuario.updatedAt = this.utily.obtenerfechaactual();
-  
+
       return await this.person.create(usuario);
     } catch (error) {
       return {
-        message: "Error con los datos creo XD"
-      }
+        message: 'Error con los datos creo XD',
+      };
     }
   }
 
   @Get()
   findAll() {
     try {
-      
       return this.person.find();
     } catch (error) {
-      return { 
-        message: "error al devolver los datos" 
-      }
+      return {
+        message: 'error al devolver los datos',
+      };
     }
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     try {
-      
       return this.person.findById(id);
     } catch (error) {
       return {
-        message: "Usuario No Encontrado"
-      }
+        message: 'Usuario No Encontrado',
+      };
     }
-
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePersonaDto: UpdatePersonaDto) {
     try {
-      
-      updatePersonaDto.updatedAt = updatePersonaDto.updatedAt = this.utily.obtenerfechaactual();
+      updatePersonaDto.updatedAt = updatePersonaDto.updatedAt =
+        this.utily.obtenerfechaactual();
       return this.person.findByIdAndUpdate(id, updatePersonaDto);
-      
     } catch (error) {
       return {
-        message: "Error al Actualizar un solo dato"
-      }
+        message: 'Error al Actualizar un solo dato',
+      };
     }
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
     try {
-      
       return this.person.remove(id);
     } catch (error) {
       return {
-        message: "Error al Eliminar un dato"
-      }
+        message: 'Error al Eliminar un dato',
+      };
     }
   }
 }
